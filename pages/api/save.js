0f3de640345bacfaf21357174b82b19e615b17bb
Felipe Costa <felipecostati@gmail.com>
2022-01-11 16:47:21 -0300

    Acrescentado get da plailha ao save,  data preenchimento e nota

--- pages/api/save.js
+++ pages/api/save.js
@@ -11,12 +11,29 @@ export default async (req, res) => {
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[1]
     const data = JSON.parse(req.body)
+
+    const sheetConfig = doc.sheetsByIndex[2]
+    await sheetConfig.loadCells('B4:B6')
+    const mostrarPromocaoCell = sheetConfig.getCell(3,1)
+    const textPromocaoCell = sheetConfig.getCell(5,1)
+
+    let Cupom = ''
+    let Promo = ''
+
+    if(mostrarPromocaoCell.value === 'VERDADEIRO') {
+      // TODO: gerar cupom
+      Cupom = 'temporario'
+      Promo = textoCell.value
+    }
+
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
-      Cupom: 'abc123',
-      Promo: 'asdfqwert'
+      Nota: 5,
+      'Data Preenchimento': moment().format('DD/MM/YYYY, HH:mm:ss'),
+      Cupom,
+      Promo
     })
     res.end(req.body)
   }catch (err){
